0b39b678508b85122f5c62f5e586d4a9860a3733
jorge <jorge@192.168.56.1>
2023-11-22 00:28:33 +0100

    Commit inicial para empezar el proyecto

---
+++ src/conectaLetra/ConectaLetra.java
@@ -0,0 +1,10 @@
+package conectaLetra;
+
+public class ConectaLetra {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola, este es el main de ConectaLetra.");
+	}
+
+}
