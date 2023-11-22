5aad1f17553e97d8c2e2735d097d124948afcd6e
jorge <jorge@192.168.56.1>
2023-11-22 01:10:41 +0100

    Segunda prueba para ver que todo esta operativo y funcional

--- src/conectaLetra/ConectaLetra.java
+++ src/conectaLetra/ConectaLetra.java
@@ -5,6 +5,8 @@ public class ConectaLetra {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hola, este es el main de ConectaLetra.");
+		System.out.println("Prueba");
+
 	}
 
 }
