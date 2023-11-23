a0fa350c44e39049744d3e3bdf4a85833a64fbb9
Administrador <Administrador@10.0.4.58>
2023-11-23 16:53:54 +0100

    Se preparan los ficheros para el proyecto

--- src/conectaLetra/ConectaLetra.java
+++ src/conectaLetra/ConectaLetra.java
@@ -3,10 +3,7 @@ package conectaLetra;
 public class ConectaLetra {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hola, este es el main de ConectaLetra.");
-		System.out.println("Prueba");
-
+		
 	}
 
 }
---
+++ src/conectaLetra/Modelo.java
@@ -0,0 +1,5 @@
+package conectaLetra;
+
+public class Modelo {
+
+}
