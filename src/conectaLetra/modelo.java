d715185c05aa2d7dc9dc87131f0c4eb4fb39b4f5
Administrador <Administrador@2DAM01>
2023-12-01 19:31:51 +0100

    Agregamos varias variables para el futuro de la aplicacion

--- src/conectaLetra/modelo.java
+++ src/conectaLetra/modelo.java
@@ -1,5 +1,15 @@
 package conectaLetra;
 
 public class modelo {
-	
+	private char[][] tablero; // Representa el tablero del juego
+    private boolean turnoJugador; // Indica de quién es el turno (true para jugador 1, false para jugador 2)
+
+    public char[][] getTablero() {
+        return tablero;
+    }
+
+    public boolean getTurnoJugador() {
+        return turnoJugador;
+    }
+    
 }
