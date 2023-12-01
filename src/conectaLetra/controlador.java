25f3d9f67e0a6e1a978c678ea85664599f9d00b8
Administrador <Administrador@2DAM01>
2023-12-01 19:32:17 +0100

    Movemos los eventos de la vista al Controlador

--- src/conectaLetra/controlador.java
+++ src/conectaLetra/controlador.java
@@ -1,5 +1,10 @@
 package conectaLetra;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 
 public class controlador  {
 
@@ -9,11 +14,26 @@ public class controlador  {
 	 public controlador(modelo miModelo, vista miVista) {
 	  this.miModelo = miModelo;
 	  this.miVista = miVista;
+	  JButton botonJugar = miVista.obtenerBotonJugar();
+	  
+	  miVista.obtenerBotonJugar().addActionListener(new ActionListener() {
+	         @Override
+	         public void actionPerformed(ActionEvent e) {
+	             // Lógica para manejar el evento de clic en el botón Jugar
+	        	 JOptionPane.showMessageDialog(null,"¡Has presionado el botón Jugar!");	         }
+	     });
+	  
 	 }
+	 
+	
 	 
 	 public void iniciarVista() {
 	  miVista.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	  miVista.setLocationRelativeTo(null);
 	  miVista.setVisible(true);  
 	 }
+	 
+	 
+	 
+
 }
--- src/conectaLetra/vista.java
+++ src/conectaLetra/vista.java
@@ -5,19 +5,21 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 
-public class vista extends JFrame{
-	
+public class vista extends JFrame {
+
+	private JButton botonJugar=new JButton();
 	
 	public vista() {
         // Configuración de la ventana
-        setTitle("Pantalla de Inicio");
+		JPanel panelPrincipal = new JPanel(new GridBagLayout());
+        panelPrincipal.setBackground(Color.WHITE);
+        
+        setTitle("Conecta Pieza");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 700);
         setMinimumSize(new Dimension(400, 400)); // Establecer el tamaño mínimo
         setLocationRelativeTo(null); // Centrar la ventana en la pantalla
 
-        // Configuración del diseño
-        getContentPane().setLayout(new GridBagLayout());
 
         JLabel lblNewLabel = new JLabel("Conecta Pieza");
         lblNewLabel.setFont(new Font("Bookman Old Style", Font.BOLD | Font.ITALIC, 44));
@@ -25,10 +27,10 @@ public class vista extends JFrame{
         gbc_lblNewLabel.insets = new Insets(0, 0, 125, 0); // Aumenta el espacio en la parte inferior
         gbc_lblNewLabel.gridx = 0;
         gbc_lblNewLabel.gridy = 0;
-        getContentPane().add(lblNewLabel, gbc_lblNewLabel);
+        panelPrincipal.add(lblNewLabel, gbc_lblNewLabel);
 
         // Crear el botón "Jugar"
-        JButton botonJugar = new JButton("Jugar");
+        botonJugar = new JButton("Jugar");
         botonJugar.setBorder(new LineBorder(new Color(0, 0, 0), 1, true));
         botonJugar.setPreferredSize(new Dimension(125, 75));  
         botonJugar.setFont(new Font("Bookman Old Style", Font.BOLD | Font.ITALIC, 32));
@@ -38,15 +40,6 @@ public class vista extends JFrame{
         // Desactivar el enfoque pintado en el texto
         botonJugar.setFocusPainted(false); 
         
-        
-        // Agregar un ActionListener al botón
-        botonJugar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // Aquí puedes agregar la lógica que se ejecutará cuando se presione el botón
-                JOptionPane.showMessageDialog(vista.this, "¡Has presionado el botón Jugar!");
-            }
-        });
 
         // Configurar restricciones para el botón
         GridBagConstraints gbc = new GridBagConstraints();
@@ -54,7 +47,12 @@ public class vista extends JFrame{
         gbc.gridy = 1; // Cambiado para colocar el botón después del JLabel
 
         // Agregar el botón al centro de la pantalla
-        getContentPane().add(botonJugar, gbc);
+        panelPrincipal.add(botonJugar, gbc);
+        
+        add(panelPrincipal);
     }
-	
+
+	public JButton obtenerBotonJugar() {
+        return botonJugar;
+    }
 }
